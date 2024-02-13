551665d59e14274fabcf951da028e4e03006573a
Brian <37948148+BiTheDev@users.noreply.github.com>
2024-02-13 12:00:08 -0800

    added few dummy data

--- data/majordata.js
+++ data/majordata.js
@@ -1,12 +1,36 @@
-const majorData =[
-    {
-        id : '1',
-        name : 'CS_Align'
-    },
-    {
-        id : '2',
-        name : 'MSCS'
-    }
+const majorData = [
+  {
+    id: "1",
+    name: "CS_Align",
+    semester: [
+      {
+        term: "Fall 2023",
+        courses: [
+          {
+            id: "5001",
+            title: "CS5001",
+            day: "Monday",
+            time: "10:00 - 13:00",
+            instructor: "Tony Mullen",
+            classroom: "225-306"
+          },
+        ],
+      },
+      {
+        term: "Fall 2024",
+        courses: [
+          {
+            id: "5001",
+            title: "CS5001",
+            day: "Tuesday",
+            time: "13:00 - 16:00",
+            instructor: "Tony Mullen",
+            classroom: "225-306"
+          },
+        ],
+      },
+    ],
+  },
 ];
 
 export default majorData;
