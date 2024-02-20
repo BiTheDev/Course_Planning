e894ceaf0cec4d50441cdbd36da5e70c0284e109
Brian <37948148+BiTheDev@users.noreply.github.com>
2024-02-19 23:31:16 -0800

    fix program model bug

--- models/Program.js
+++ models/Program.js
@@ -9,6 +9,8 @@ const ProgramSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'Course',
   }],
+  createdBy: { type: Schema.Types.ObjectId, ref: "Admin" },
+  updatedBy: { type: Schema.Types.ObjectId, ref: "Admin" },
 });
 
 const Program = models.Program || model('Program', ProgramSchema);
