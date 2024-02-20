9e97b277faef8bb0970a9a9c5e3372d6bb940b79
Brian <37948148+BiTheDev@users.noreply.github.com>
2024-02-20 12:30:17 -0800

    update navbar with admin check

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,9 +2,16 @@
 "use client";
 import Link from "next/link";
 import { useMajor } from "@/components/MajorProvider";
+import { useRouter } from "next/navigation";
 
 const Nav = () => {
-  const { major } = useMajor();
+  const { major, admin, updateAdmin } = useMajor();
+  const router = useRouter();
+  const handleLogout = () => {
+    updateAdmin(null); // Clear the admin state
+    localStorage.removeItem("admin"); // Clear admin info from localStorage if used for persistence
+    router.push("/"); // Redirect to the login page or home page after logout
+  };
   return (
     <nav className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
@@ -12,27 +19,38 @@ const Nav = () => {
           NEU
         </Link>
         <div className="hidden md:flex space-x-4">
-        <Link
-            href="/major-overview"
-            className="hover:text-gray-300"
-          >
+          <Link href="/major-overview" className="hover:text-gray-300">
             Major Overview
           </Link>
-          <Link
-            href="/course-management"
-            className="hover:text-gray-300"
-          >
-            Course Management
-          </Link>
-          <Link href="/instructor-management" className="hover:text-gray-300">
-            Instructor Management
-          </Link>
+          {admin && (
+            <Link href="/course-management" className="hover:text-gray-300">
+              Course Management
+            </Link>
+          )}
+          {admin && (
+            <Link href="/instructor-management" className="hover:text-gray-300">
+              Instructor Management
+            </Link>
+          )}
           <Link href="/classroom-allocation" className="hover:text-gray-300">
             Classroom Allocation
           </Link>
           <Link href="/master-schedule" className="hover:text-gray-300">
             Master Schedule
           </Link>
+          {admin && (
+            <Link href="/settings" className="hover:text-gray-300">
+              Settings
+            </Link>
+          )}
+          {admin && (
+            <button
+              onClick={handleLogout}
+              className="bg-red-500 hover:bg-red-700 text-white font-bold px-2 rounded focus:outline-none focus:shadow-outline"
+            >
+              Logout
+            </button>
+          )}
         </div>
         {/* Mobile menu button */}
         <button className="md:hidden text-xl">
