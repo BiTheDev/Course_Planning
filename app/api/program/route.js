6791ba9fd0c2f035c7beb61b4cd278a2473bd6f7
Brian <37948148+BiTheDev@users.noreply.github.com>
2024-02-20 22:15:08 -0800

    basic get program completed

--- app/api/program/route.js
+++ app/api/program/route.js
@@ -1,30 +1,16 @@
 import { connectToDB } from "@/utils/mongodbUtil";
 import Program from "@/models/Program";
-import Admin from "@/models/Admin";
 
-export const POST = async (request) => {
-    const { title, adminName } = await request.json();
-
+export const GET = async (request) => {
     try {
         await connectToDB();
 
-        // Verify admin exists
-        const admin = await Admin.findOne({username: adminName});
-        if (!admin) {
-            return new Response(JSON.stringify({ error: 'Admin not found' }), { status: 404 });
-        }
+        // Retrieve all programs from the database
+        const programs = await Program.find({}); 
 
-        const newProgram = await Program.create({
-            title,
-            createdBy: admin._id,
-            updatedBy: admin._id,
-        });
-
-        return new Response(JSON.stringify(newProgram), { status: 201 });
+        return new Response(JSON.stringify(programs), { status: 200 });
     } catch (error) {
-        console.error(error);
+        console.error(error); // Log the error for debugging
         return new Response(JSON.stringify({ error: 'Server error' }), { status: 500 });
     }
 };
-
-// Include a PUT method if you want to handle updates as well
