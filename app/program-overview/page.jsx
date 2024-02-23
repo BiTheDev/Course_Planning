6d06f941b5d425e6e1735ae10ff78b2db7d2131f
Brian <37948148+BiTheDev@users.noreply.github.com>
2024-02-22 21:25:33 -0800

    cleaned program overview page

--- app/program-overview/page.jsx
+++ app/program-overview/page.jsx
@@ -2,14 +2,15 @@
 import { useEffect, useState } from "react";
 import MainLayout from "../MainLayout";
 import { useMajor } from "@/components/MajorProvider";
+import ProgramDropdown from "@/components/ProgramDropdown"; // Import the new component
 
 const MajorOverview = () => {
-  const { updateProgram, program } = useMajor(); // Use updateProgram to set the selected program
+  const { updateProgram, program } = useMajor();
   const [programs, setPrograms] = useState([]);
 
   useEffect(() => {
     const fetchPrograms = async () => {
-      const response = await fetch('/api/program'); // Adjust the API endpoint as necessary
+      const response = await fetch('/api/program');
       const data = await response.json();
       setPrograms(data);
     };
@@ -17,31 +18,18 @@ const MajorOverview = () => {
   }, []);
 
   const handleProgramChange = (selectedProgramId) => {
-    console.log(selectedProgramId);
     const selectedProgram = programs.find(p => p._id === selectedProgramId);
     updateProgram(selectedProgram); // Update the selected program in the context
-    console.log(program);
   };
 
   return (
     <MainLayout>
       <div className="container mx-auto px-4 py-8">
-        <div className="mb-4">
-          <label htmlFor="program-select" className="block mb-2 text-sm font-medium text-gray-900">Select a Program:</label>
-          <select
-            id="program-select"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            value={program?.id || ''}
-            onChange={(e) => handleProgramChange(e.target.value)}
-          >
-            <option value="">Select a program</option>
-            {programs.map((p) => (
-              <option key={p._id} value={p._id}>
-                {p.title}
-              </option>
-            ))}
-          </select>
-        </div>
+        <ProgramDropdown
+          programs={programs}
+          selectedProgram={program}
+          onProgramChange={handleProgramChange}
+        />
 
         {program && (
           <div>
