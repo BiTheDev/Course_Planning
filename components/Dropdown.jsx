4d0ea2d7b7c12f37d089565705c52e1bf9d67d39
Brian <37948148+BiTheDev@users.noreply.github.com>
2024-02-25 22:51:12 -0800

    added custom params to dropdown component

--- components/Dropdown.jsx
+++ components/Dropdown.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Dropdown = ({ data, selectedData, onDataChange, dropDownType, labelProperty }) => {
   return (
     <div className="mb-4">
-      <label htmlFor="program-select" className="block mb-2 text-sm font-medium text-gray-900">Select a Program:</label>
+      <label htmlFor="program-select" className="block mb-2 text-sm font-medium text-gray-900">Select a {dropDownType}:</label>
       <select
         id="program-select"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
