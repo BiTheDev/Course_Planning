56da0c4d64f37ebf9721b5991a7b1883ebd82c38
Brian <37948148+BiTheDev@users.noreply.github.com>
2024-02-26 01:04:05 -0800

    added courses filter that already in the selected semester

---
+++ app/api/semester/[id]/route.js
@@ -0,0 +1,19 @@
+import { connectToDB } from "@/utils/mongodbUtil";
+import Semester from "@/models/Semester";
+export const GET = async (request, {params}) =>{
+    try {
+        await connectToDB();
+
+        const getSemesterCourses = await Semester.findById(params.id).populate("courses");
+
+        if (!getSemesterCourses) {
+            return new Response(JSON.stringify({ error: 'Semester id not found' }), { status: 404 });
+        }
+
+
+        return new Response(JSON.stringify(getSemesterCourses), { status: 200 });
+    } catch (error) {
+        console.error(error);
+        return new Response(JSON.stringify({ error: 'Server error' }), { status: 500 });
+    }
+}
