89010d9168b4cd61164235b4190e4d3e4807290e
Brian <37948148+BiTheDev@users.noreply.github.com>
2024-02-25 16:03:30 -0800

    update dropdown component structure and added select term

--- app/program-overview/page.jsx
+++ app/program-overview/page.jsx
@@ -2,15 +2,17 @@
 import { useEffect, useState } from "react";
 import MainLayout from "../MainLayout";
 import { useMajor } from "@/components/MajorProvider";
-import ProgramDropdown from "@/components/ProgramDropdown"; // Import the new component
+import Dropdown from "@/components/Dropdown"; // Import the new component
 
 const MajorOverview = () => {
-  const { updateProgram, program } = useMajor();
+  const { updateProgram, program, fetchSemesterOnProgram, semesters } =
+    useMajor();
   const [programs, setPrograms] = useState([]);
+  const [semester, setSemester] = useState(null);
 
   useEffect(() => {
     const fetchPrograms = async () => {
-      const response = await fetch('/api/program');
+      const response = await fetch("/api/program");
       const data = await response.json();
       setPrograms(data);
     };
@@ -18,23 +20,44 @@ const MajorOverview = () => {
   }, []);
 
   const handleProgramChange = (selectedProgramId) => {
-    const selectedProgram = programs.find(p => p._id === selectedProgramId);
-    updateProgram(selectedProgram); // Update the selected program in the context
+    const selectedProgram = programs.find((p) => p._id === selectedProgramId);
+    console.log(selectedProgram);
+    updateProgram(selectedProgram);
+    fetchSemesterOnProgram(selectedProgram._id);
   };
+
+  const handleSemesterChange = (selectedSemesterId) => {
+    console.log(selectedSemesterId);
+  };
+
 
   return (
     <MainLayout>
       <div className="container mx-auto px-4 py-8">
-        <ProgramDropdown
-          programs={programs}
-          selectedProgram={program}
-          onProgramChange={handleProgramChange}
+        <Dropdown
+          data={programs}
+          selectedData={program}
+          onDataChange={handleProgramChange}
+          dropDownType="Program"
+          labelProperty="title"
         />
 
         {program && (
           <div>
-            <p className="mb-4">You are currently viewing the program: <strong>{program.title}</strong></p>
-            {/* Additional details and operations related to the selected program can be added here */}
+            <p className="mb-4">
+              You are currently viewing the program:{" "}
+              <strong>{program.title}</strong>
+            </p>
+            <p className="mb-4">
+              Please select a semester to view course details
+            </p>
+            <Dropdown
+              data={semesters}
+              selectedData={semester}
+              onDataChange={handleSemesterChange}
+              dropDownType="Semester"
+              labelProperty="term"
+            />
           </div>
         )}
       </div>
