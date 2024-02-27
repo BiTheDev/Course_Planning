92bf4686768311b634b138a2028e6d0e50bd5d68
Brian <37948148+BiTheDev@users.noreply.github.com>
2024-02-27 00:13:19 -0800

    added missing schema import

--- app/api/course/route.js
+++ app/api/course/route.js
@@ -1,11 +1,11 @@
 import { connectToDB } from "@/utils/mongodbUtil";
 import Course from "@/models/Course";
-
+import Semester from "@/models/Semester";
 export const GET = async (request) => {
     try {
         await connectToDB();
 
-        const courses = await Course.find({})
+        const courses = await Course.find({}).populate("semesters");
         return new Response(JSON.stringify(courses), { status: 200 });
     } catch (error) {
         console.error(error);
