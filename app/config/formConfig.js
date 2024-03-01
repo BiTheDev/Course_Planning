99ed99095eb0da5bdc56e1b0652cfef93233fcd0
Brian <37948148+BiTheDev@users.noreply.github.com>
2024-03-01 04:03:01 -0800

    added Semester Form config to Dynamic Create Form

--- app/config/formConfig.js
+++ app/config/formConfig.js
@@ -1,56 +1,68 @@
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 export const programFormConfig = {
- formTitle: "Create Program",
+  formTitle: "Create Program",
   initialValues: {
-    title: '',
+    title: "",
   },
   validationSchema: Yup.object({
-    title: Yup.string().required('Program title is required'),
+    title: Yup.string().required("Program title is required"),
   }),
-  fields: [
-    { name: 'title', type: 'text', label: 'Program Title' },
-  ],
-  apiEndpoint: '/api/program/create',
-  successMessage: 'Program created successfully!',
-  errorMessage: 'Error creating program. Please try again.'
+  fields: [{ name: "title", type: "text", label: "Program Title" }],
+  apiEndpoint: "/api/program/create",
+  successMessage: "Program created successfully!",
+  errorMessage: "Error creating program. Please try again.",
 };
 
 export const courseFormConfig = {
-    formTitle: "Create Course",
+  formTitle: "Create Course",
   initialValues: {
-    identifyCode: '',
+    identifyCode: "",
     maxSections: 1,
   },
   validationSchema: Yup.object({
-    identifyCode: Yup.string().required('Identify code is required'),
-    maxSections: Yup.number().required('Max sections are required').min(1),
+    identifyCode: Yup.string().required("Identify code is required"),
+    maxSections: Yup.number().required("Max sections are required").min(1),
   }),
   fields: [
-    { name: 'identifyCode', type: 'text', label: 'Identify Code' },
-    { name: 'maxSections', type: 'number', label: 'Max Sections' },
+    { name: "identifyCode", type: "text", label: "Identify Code" },
+    { name: "maxSections", type: "number", label: "Max Sections" },
   ],
-  apiEndpoint: '/api/course/create',
-  successMessage: 'Course created successfully!',
-  errorMessage: 'Error creating course. Please try again.'
+  apiEndpoint: "/api/course/create",
+  successMessage: "Course created successfully!",
+  errorMessage: "Error creating course. Please try again.",
 };
 
 export const instructorFormConfig = {
-    formTitle: "Create Instructor",
+  formTitle: "Create Instructor",
   initialValues: {
-    name: '',
+    name: "",
     maxCourses: 1,
     preferredTime: [],
   },
   validationSchema: Yup.object({
-    name: Yup.string().required('Instructor name is required'),
-    maxCourses: Yup.number().required('Max courses are required').min(1),
+    name: Yup.string().required("Instructor name is required"),
+    maxCourses: Yup.number().required("Max courses are required").min(1),
   }),
   fields: [
-    { name: 'name', type: 'text', label: 'Name' },
-    { name: 'maxCourses', type: 'number', label: 'Max Courses' },
+    { name: "name", type: "text", label: "Name" },
+    { name: "maxCourses", type: "number", label: "Max Courses" },
   ],
-  apiEndpoint: '/api/instructor/create',
-  successMessage: 'Instructor created successfully!',
-  errorMessage: 'Error creating instructor. Please try again.'
+  apiEndpoint: "/api/instructor/create",
+  successMessage: "Instructor created successfully!",
+  errorMessage: "Error creating instructor. Please try again.",
+};
+
+export const semesterFormConfig = {
+  formTitle: "Create Semester",
+  initialValues: {
+    term: "",
+  },
+  validationSchema: Yup.object({
+    term: Yup.string().required("Semester term is required"),
+  }),
+  fields: [{ name: "title", type: "text", label: "Program Title", placeholder: "e.g., Fall 2024" }],
+  apiEndpoint: "/api/semester/create",
+  successMessage: "Semester created successfully!",
+  errorMessage: "Error creating semester. Please try again.",
 };
--- components/Forms/CreateForms/DynamicCreateForm.jsx
+++ components/Forms/CreateForms/DynamicCreateForm.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useMajor } from "@/components/General/MajorProvider";
 const DynamicCreateForm = ({
-    formTitle,
+  formTitle,
   initialValues,
   validationSchema,
   fields,
@@ -11,13 +11,28 @@ const DynamicCreateForm = ({
   errorMessage,
   onFormSubmitted,
 }) => {
-    const {admin} = useMajor();
+  const { admin, program, fetchSemesterOnProgram } = useMajor();
+
+  let isSemesterForm = formTitle == "Create Semester";
 
   const handleSubmit = async (values, actions) => {
-    const data = {
+    let data = {};
+    if (isSemesterForm && (!admin || !program)) {
+      alert(
+        "Admin or Program information is missing. Please ensure you are logged in and a program is selected."
+      );
+      actions.setSubmitting(false);
+      return;
+    }
+    if (isSemesterForm) {
+      data = { ...values, programId: program._id, adminName: admin.username };
+    } else {
+      data = {
         ...values,
         adminName: admin.username,
       };
+    }
+
     try {
       const response = await fetch(apiEndpoint, {
         method: "POST",
@@ -28,9 +43,11 @@ const DynamicCreateForm = ({
       });
 
       if (!response.ok) throw new Error("Network response was not ok");
-      alert(successMessage);
+
       if (onFormSubmitted) onFormSubmitted();
+      if (isSemesterForm) fetchSemesterOnProgram(program._id);
       actions.resetForm();
+      alert(successMessage);
     } catch (error) {
       console.error("Error:", error);
       alert(errorMessage);
@@ -39,10 +56,14 @@ const DynamicCreateForm = ({
     actions.setSubmitting(false);
   };
 
+  if (isSemesterForm && !program) {
+    return <div>Please select a program to add a semester to.</div>;
+  }
+
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <h1 className="text-2xl mb-6">{formTitle}</h1>
+        <h1 className="text-2xl mb-6">{formTitle}</h1>
         <Formik
           initialValues={initialValues}
           validationSchema={validationSchema}
@@ -62,6 +83,7 @@ const DynamicCreateForm = ({
                     id={field.name}
                     name={field.name}
                     type={field.type}
+                    placeholder={field?.placeholder}
                     className="py-2 px-4 border border-gray-300 rounded-md shadow-sm w-full"
                   />
                   <ErrorMessage
