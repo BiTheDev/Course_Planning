989b9dc8436f57fb4b40453a673f3b807d81ab3b
Brian <37948148+BiTheDev@users.noreply.github.com>
2024-04-11 14:11:08 -0700

    get schedule api and schedule viewer in progress

--- app/api/schedule/route.js
+++ app/api/schedule/route.js
@@ -1,4 +1,3 @@
-// pages/api/generateScheduleSet.js
 import { connectToDB } from "@/utils/mongodbUtil";
 import Program from "@/models/Program";
 import Semester from "@/models/Semester";
@@ -6,73 +5,6 @@ import Schedule from "@/models/Schedule";
 import ScheduleSet from "@/models/ScheduleSet";
 import Classroom from "@/models/Classroom";
 
-export const POST = async (request) => {
-  const { programId, semesterId, generatedSchedules } = req.body;
-
-  try {
-    await connectToDB();
-
-    // Verify program and semester exist
-    const program = await Program.findById(programId);
-    const semester = await Semester.findById(semesterId);
-    if (!program || !semester) {
-      return res.status(404).json({ error: "Program or Semester not found" });
-    }
-
-    // Create Schedule documents
-    const scheduleDocuments = await Promise.all(
-      generatedSchedules.map(async (generatedSchedule) => {
-        // Split the room property to extract building and roomNumber
-        const [building, roomNumber] = generatedSchedule.room.split("-");
-
-        // Find the matching Classroom
-        const classroom = await Classroom.findOne({ building, roomNumber });
-        if (!classroom) {
-          // Handle the case where no matching classroom is found
-          console.error(
-            `No matching classroom found for ${generatedSchedule.room}`
-          );
-          return null; // Or handle differently based on your requirements
-        }
-
-        // Create the Schedule document with a reference to the found Classroom
-        return Schedule.create({
-          course: generatedSchedule.course,
-          professor: generatedSchedule.professor,
-          startDay: generatedSchedule.startDay,
-          startTime: generatedSchedule.startTime,
-          endTime: generatedSchedule.endTime,
-          duration: generatedSchedule.duration,
-          criteriaSatisfied: generatedSchedule.criteriaSatisfied,
-          classroom: classroom._id, // Reference the Classroom document
-        });
-      })
-    );
-    // Create a new ScheduleSet with references to the created schedules, program, and semester
-    const newScheduleSet = await ScheduleSet.create({
-      program: programId,
-      semester: semesterId,
-      schedules: scheduleDocuments.map((doc) => doc._id),
-    });
-
-    semester.scheduleSet.push(newScheduleSet._id);
-    await semester.save();
-
-    return res
-      .status(201)
-      .json({
-        success: "ScheduleSet created successfully",
-        scheduleSet: newScheduleSet,
-      });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Server error" });
-  }
-};
-
-// pages/api/getScheduleSets.js
-import { connectToDB } from "@/utils/mongodbUtil";
-import ScheduleSet from "@/models/ScheduleSet";
 export const GET = async (request) => {
   // Extract programId and semesterId from query parameters
   const { programId, semesterId } = req.query;
@@ -96,17 +28,9 @@ export const GET = async (request) => {
       })
       .exec();
 
-    if (!scheduleSets.length) {
-      return res
-        .status(404)
-        .json({
-          error: "No schedule sets found for the given program and semester.",
-        });
-    }
-
-    return res.status(200).json(scheduleSets);
+    return new Response(JSON.stringify({ scheduleSets }), { status: 200 });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: "Server error" });
+    return new Response(JSON.stringify({ error: 'Server error' }), { status: 500 });
   }
 };
