ea2fd0df3a1de8efd38759aedb67efcf6c0b7a36
Brian <37948148+BiTheDev@users.noreply.github.com>
2024-04-11 14:10:49 -0700

    added api to store schedule data to database

--- components/DynamicSemesterInfoList.jsx
+++ components/DynamicSemesterInfoList.jsx
@@ -1,6 +1,6 @@
 // DynamicSemesterInfoList.js
 "use client";
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useMajor } from "./General/MajorProvider";
 
 const DynamicSemesterInfoList = ({
@@ -19,10 +19,23 @@ const DynamicSemesterInfoList = ({
     fetchAllCourses,
     fetchAllSections,
     allClassrooms,
+    semester,
+    program,
   } = useMajor();
-
+  const [isLoading, setIsLoading] = useState(false);
   const [scheduleData, setScheduleData] = useState([]); // 存储 GET 请求结果
-  const scheduleHeaders = ["Section ID", "Course Title", "Professor", "Day", "Start Time", "End Time", "Room Number", "Duration(min)", "Relative Start", "Conflict"];
+  const scheduleHeaders = [
+    "Section ID",
+    "Course Title",
+    "Professor",
+    "Day",
+    "Start Time",
+    "End Time",
+    "Room Number",
+    "Duration(min)",
+    "Relative Start",
+    "Conflict",
+  ];
 
   useEffect(() => {
     if (ListType === "courses") {
@@ -61,6 +74,7 @@ const DynamicSemesterInfoList = ({
     }
   };
   const handleGenerate = async () => {
+    setIsLoading(true);
     // Assuming `semesterSections` and `allClassrooms` contain the data in MongoDB format
     const sections = semesterSections.map((section) => ({
       course: section.courseCode,
@@ -94,7 +108,8 @@ const DynamicSemesterInfoList = ({
         body: JSON.stringify(payload),
       });
 
-      if (!postResponse.ok) throw new Error(" POST Network response was not ok");
+      if (!postResponse.ok)
+        throw new Error(" POST Network response was not ok");
 
       // Optionally process the POST response
       const postResult = await postResponse.json();
@@ -112,15 +127,18 @@ const DynamicSemesterInfoList = ({
       if (!getResponse.ok) throw new Error("GET Network response was not ok");
 
       const getResult = await getResponse.json();
-      console.log(getResult); // Process the GET response
-
-      // Here you can update your state or UI based on the getResult
+      console.log(getResult);
+      setIsLoading(false);
+      handleStoreToDatabase(getResult);
+      // setScheduleData(getResult);
     } catch (error) {
       console.error("Error:", error);
+      setIsLoading(false);
     }
   };
 
-  const handleGetNewSchedule = async() =>{
+  const handleGetNewSchedule = async () => {
+    setIsLoading(true);
     try {
       // GET request
       const getResponse = await fetch("api/proxy", {
@@ -134,12 +152,42 @@ const DynamicSemesterInfoList = ({
       if (!getResponse.ok) throw new Error("GET Network response was not ok");
 
       const getResult = await getResponse.json();
-      console.log(getResult); // Process the GET response
-      setScheduleData(getResult);// 更新状态，存储 API 返回的数据
+      console.log(getResult);
+      setIsLoading(false);
+      handleStoreToDatabase(getResult);
+      setScheduleData(getResult);
     } catch (error) {
       console.error("Error:", error);
+      alert("Error generating schedule. Please try again.");
+      setIsLoading(false);
     }
-  }
+  };
+
+  const handleStoreToDatabase = async (getResult) => {
+    try {
+      const storeResponse = await fetch("/api/createScheduleSet", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          programId: program._id,
+          semesterId: semester._id,
+          generatedSchedules: getResult,
+        }),
+      });
+      if (!storeResponse.ok)
+        throw new Error(
+          "POST Network response was not ok when storing schedule"
+        );
+
+      const result = await storeResponse.json();
+      console.log(result); // Process/store this result as necessary
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Error generating schedule. Please try again.");
+    }
+  };
 
   return (
     <div className="container mx-auto p-4">
@@ -150,124 +198,140 @@ const DynamicSemesterInfoList = ({
       <div className="overflow-x-auto">
         <table className="min-w-full divide-y divide-gray-200">
           <thead className="bg-gray-50">
-          <tr>
-            {ListColumns.map((column) => (
+            <tr>
+              {ListColumns.map((column) => (
                 <th
-                    key={column.key}
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  key={column.key}
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                 >
                   {column.header}
                 </th>
-            ))}
-            <th
+              ))}
+              <th
                 scope="col"
                 className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-            >
-              Actions
-            </th>
-          </tr>
+              >
+                Actions
+              </th>
+            </tr>
           </thead>
           <tbody className="bg-white divide-y divide-gray-200">
-          {data &&
+            {data &&
               data.map((item) => (
-                  <tr key={item._id}>
-                    {ListColumns.map((column) => (
-                        <td
-                            key={column.key}
-                            className="px-6 py-4 whitespace-nowrap"
-                        >
-                          <div className="text-sm text-gray-900">
-                            {column.render ? column.render(item) : item[column.key]}
-                          </div>
-                        </td>
-                    ))}
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <button
-                          onClick={() => handleSubmit(item._id)}
-                          className="text-blue-600 hover:text-blue-900 mr-2"
-                      >
-                        Edit
-                      </button>
-                      <button
-                          onClick={() => handleDelete(item._id)}
-                          className="text-red-600 hover:text-red-900"
-                      >
-                        Delete
-                      </button>
+                <tr key={item._id}>
+                  {ListColumns.map((column) => (
+                    <td
+                      key={column.key}
+                      className="px-6 py-4 whitespace-nowrap"
+                    >
+                      <div className="text-sm text-gray-900">
+                        {column.render ? column.render(item) : item[column.key]}
+                      </div>
                     </td>
-                  </tr>
+                  ))}
+                  <td className="px-6 py-4 whitespace-nowrap">
+                    <button
+                      onClick={() => handleSubmit(item._id)}
+                      className="text-blue-600 hover:text-blue-900 mr-2"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => handleDelete(item._id)}
+                      className="text-red-600 hover:text-red-900"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
               ))}
           </tbody>
         </table>
       </div>
       {(!data || data.length === 0) && (
-          <div className="text-center py-4">
-            <p className="text-gray-500">No {ListType} found.</p>
-          </div>
+        <div className="text-center py-4">
+          <p className="text-gray-500">No {ListType} found.</p>
+        </div>
       )}
 
       {/*-------------generate schedule buttons------------*/}
 
       {ListType === "semesterSections" && (
-          <div className="mb-4">
-            <button
-                onClick={() => handleGenerate(data)}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              {/*First time Generate*/}
-              Send Data
-            </button>
-          </div>
+        <div className="mb-4">
+          <button
+            onClick={() => handleGenerate(data)}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            {/*First time Generate*/}
+            Send Data
+          </button>
+        </div>
       )}
       {ListType === "semesterSections" && (
-          <div className="mb-4">
-            <button
-                onClick={() => handleGetNewSchedule()}
-                className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              Get New Schedule
-            </button>
-          </div>
+        <div className="mb-4">
+          <button
+            onClick={() => handleGetNewSchedule()}
+            className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Get New Schedule
+          </button>
+        </div>
+      )}
+
+      {isLoading && (
+        <div className="text-center">
+          <p>Loading schedules...</p>
+          {/* Or insert a spinner/loader component here */}
+        </div>
       )}
 
       {/*------------ show the generated schedule------------*/}
       {ListType === "semesterSections" && (
         <div className="overflow-x-auto">
-        <table className="min-w-full divide-y divide-gray-200">
-          <thead className="bg-gray-50">
-          <tr>
-            {scheduleHeaders.map((header, index) => (
-                <th key={index}
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  {header}
-                </th>
-            ))}
-          </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-          {scheduleData.map((item, index) => (
-              <tr key={index}>
-                {Object.entries(item).map(([key, value], idx, array) => (
-                    <td key={idx} className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                      {/* 检查是否是最后一列 */}
-                      {idx === array.length - 1
-                          ? (value === false
-                              ? <span className="text-red-600">conflict</span> // 如果值为false，则显示红色的"conflict"
-                              : '') // 如果值为true，则不显示任何内容
-                          : (typeof value === 'boolean'
-                              ? value.toString() // 如果值是布尔类型，则将其转换为字符串
-                              : value) // 其他情况正常显示值
-                      }
-                    </td>
+          <table className="min-w-full divide-y divide-gray-200">
+            <thead className="bg-gray-50">
+              <tr>
+                {scheduleHeaders.map((header, index) => (
+                  <th
+                    key={index}
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    {header}
+                  </th>
                 ))}
               </tr>
-          ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              {scheduleData.map((item, index) => (
+                <tr key={index}>
+                  {Object.entries(item).map(([key, value], idx, array) => (
+                    <td
+                      key={idx}
+                      className="px-6 py-4 whitespace-nowrap text-sm text-gray-900"
+                    >
+                      {/* 检查是否是最后一列 */}
+                      {
+                        idx === array.length - 1 ? (
+                          value === false ? (
+                            <span className="text-red-600">conflict</span> // 如果值为false，则显示红色的"conflict"
+                          ) : (
+                            ""
+                          ) // 如果值为true，则不显示任何内容
+                        ) : typeof value === "boolean" ? (
+                          value.toString() // 如果值是布尔类型，则将其转换为字符串
+                        ) : (
+                          value
+                        ) // 其他情况正常显示值
+                      }
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       )}
-      
     </div>
   );
 };
